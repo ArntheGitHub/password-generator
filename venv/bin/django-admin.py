013f3e1556b699f267ef0a26f917bbe0f27df03b
ArnTheGitHub <olympio116@yahoo.fr>
2020-10-14 22:07:46 +0200

    password generator with python&django

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Olympio/PycharmProjects/password_generator-project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
